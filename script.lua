58dbbb9cd96e74e12f5a6f61fbce43f28fdaaf4e
xLDoMx <luke.raffermatix@gmail.com>
2026-08-02 01:43:44 +0100

    Update script description for Enchant Station coords
    Pleasseee update

--- script.lua
+++ script.lua
@@ -1,5 +1,5 @@
 --======================================================================================
--- DOMINATE HUB | FULL SCRIPT (WITH REALM 2 & ENCHANT STATION COORDS SET1)
+-- DOMINATE HUB | FULL SCRIPT (WITH REALM 2 & ENCHANT STATION COORDS SET2)
 --======================================================================================
 if getgenv().DominateHubLoaded then 
     print("[Dominate Hub] Already running! Aborting duplicate instance.")
