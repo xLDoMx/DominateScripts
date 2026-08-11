40232009ef0726fcf3f86d29653154916cb8484f
xLDoMx <luke.raffermatix@gmail.com>
2026-08-11 19:16:16 +0100

    Update version from Updatex2 to Updatex3

--- script.lua
+++ script.lua
@@ -1,5 +1,5 @@
 --======================================================================================
--- DOMINATE HUB | PRO EDITION (STABLE V16.9.103 - Updatex2)
+-- DOMINATE HUB | PRO EDITION (STABLE V16.9.103 - Updatex3)
 --======================================================================================
 local Env = (getgenv and getgenv()) or _G
 
@@ -2551,8 +2551,7 @@ task.spawn(function()
                         dir = Vector3.new(dir.X, 0, dir.Z)
                         if dir.Magnitude > 0.1 then
                             -- Stand 1.0 stud away from the ore center, locking vertical height to player's current floor level to prevent sinking
-                            local targetXZ = orePos + (dir.Unit * 1.0)
-                            MiningTargetVector = Vector3.new(targetXZ.X, hrp.Position.Y, targetXZ.Z)
+                            MiningTargetVector = orePos
                         else
                             MiningTargetVector = Vector3.new(orePos.X, hrp.Position.Y, orePos.Z)
                         end
